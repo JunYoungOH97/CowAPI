c21a4a5ca16b8b755dc2132059b7d5489f0d8b85
Oh-JunYoung <qawesdxc@naver.com>
2022-04-24 15:45:54 +0900

    Test : files for service test
    MessageService 를 개발하기 전 서비스 테스트 코드 추가

--- server/src/main/java/toyspringboot/server/Service/MessageService.java
+++ server/src/main/java/toyspringboot/server/Service/MessageService.java
@@ -2,10 +2,36 @@ package toyspringboot.server.Service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import toyspringboot.server.Domain.Dto.MessageDto;
+import toyspringboot.server.Domain.Entity.Messages;
+import toyspringboot.server.Domain.Repository.MessageRepository;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class MessageService {
+    private final MessageRepository messageRepository;
+
+    public MessageDto createMessage(MessageDto messageDto) {
+        return messageDto;
+    }
+
+    public MessageDto searchMessage(Long id) {
+        return MessageDto.builder().id(id).build();
+    }
+
+    public MessageDto updateMessage(Long id, String message) {
+        return MessageDto.builder()
+                .id(id)
+                .message(message)
+                .build();
+    }
+
+    public MessageDto deleteMessage(Long id) {
+        return MessageDto.builder()
+                .id(id)
+                .build();
+    }
 
 }
