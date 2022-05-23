7415c69c7797620dbb2af0058ff2903116b72ccb
ckgod <rhckdrnr123@gmail.com>
2022-05-23 14:49:06 +0900

    Feat : QnA controller for create QnA
    QnA 생성을 위한 controller 구현 및 테스트 통과

---
+++ server/src/main/java/toyspringboot/server/Controller/QnAController.java
@@ -0,0 +1,32 @@
+package toyspringboot.server.Controller;
+
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import toyspringboot.server.Domain.Dto.QnADto;
+import toyspringboot.server.Domain.Dto.UserDto;
+import toyspringboot.server.Domain.Entity.QnA;
+import toyspringboot.server.Service.QnAService;
+import toyspringboot.server.Service.UserService;
+
+@Api(tags = {"QnA 게시판"})
+@RequestMapping(value = "/api/v1")
+@RestController
+@RequiredArgsConstructor
+public class QnAController {
+    private final QnAService qnAService;
+
+    @ApiOperation(value = "QnA 생성", notes = "QnA 게시글 생성 API 입니다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "success"),
+            @ApiResponse(code = 404, message = "존재하지 않는 사용자 입니다."),
+    })
+    @PostMapping("/QnAs/QnA")
+    public QnADto createQnA(@RequestHeader("Authorization") String userToken, @RequestBody QnADto qnADto) {
+        return qnAService.createQnA(userToken, qnADto);
+    }
+}
---
+++ server/src/test/java/toyspringboot/server/QnA/QnAControllerTest.java
@@ -0,0 +1,35 @@
+package toyspringboot.server.QnA;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import toyspringboot.server.Domain.Dto.QnADto;
+import toyspringboot.server.Domain.Dto.UserDto;
+import toyspringboot.server.TestModule.ControllerTest;
+
+import static toyspringboot.server.User.UserTestConstants.*;
+import static toyspringboot.server.User.UserTestConstants.SignUp_API;
+import static toyspringboot.server.QnA.QnATestConstants.*;
+
+public class QnAControllerTest extends ControllerTest {
+    @Test
+    @DisplayName("[Controller] QnA 생성 테스트")
+    public void createTest() throws Exception {
+        // given
+        String request = getRequestJson(QnADto.builder()
+                .title(QnA_title)
+                .content(QnA_content)
+                .build()
+        );
+
+        String header = Exist_User_email;
+
+        // when
+        ResultActions actions = postRequest(baseUrl(QnA_create_API), header, request, MediaType.APPLICATION_JSON);
+
+        // then
+        actions.andExpect(MockMvcResultMatchers.status().isOk());
+    }
+}
