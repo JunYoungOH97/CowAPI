95213b03fb1232a18fb9ec5544f18278a9788407
oh-junyoung <qawesdxc@naver.com>
2022-05-27 00:07:29 +0900

    Refactor : User service test for right test code
    SpringbootTest 였던 테스트 코드 수정

--- server/src/test/java/toyspringboot/server/TestConfig/ControllerTest.java
+++ server/src/test/java/toyspringboot/server/TestConfig/ControllerTest.java
@@ -1,4 +1,8 @@
 package toyspringboot.server.TestConfig;
 
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.transaction.annotation.Transactional;
+
 public class ControllerTest {
 }
--- server/src/test/java/toyspringboot/server/TestConfig/ServiceTest.java
+++ server/src/test/java/toyspringboot/server/TestConfig/ServiceTest.java
@@ -1,4 +1,11 @@
 package toyspringboot.server.TestConfig;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import javax.transaction.Transactional;
+
+@ExtendWith(MockitoExtension.class)
+@Transactional
 public class ServiceTest {
 }
