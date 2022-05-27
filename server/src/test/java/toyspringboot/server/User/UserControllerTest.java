04fbe253fb03bdeb3caae79a9f58c8398c362485
oh-junyoung <qawesdxc@naver.com>
2022-05-27 15:08:36 +0900

    Refactor : User controller test for webmvctest
    WebMvcTest 수정, 코드 미완성, 임시 저장

--- server/src/test/java/toyspringboot/server/User/UserControllerTest.java
+++ server/src/test/java/toyspringboot/server/User/UserControllerTest.java
@@ -1,50 +1,40 @@
 package toyspringboot.server.User;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import toyspringboot.server.Controller.UserController;
 import toyspringboot.server.Domain.Dto.UserDto;
 import toyspringboot.server.Service.UserService;
 import toyspringboot.server.TestConfig.ControllerTest;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 import static toyspringboot.server.User.UserTestConstants.*;
-import static org.junit.jupiter.api.Assertions.*;
+
 
 @WebMvcTest(UserController.class)
 public class UserControllerTest extends ControllerTest {
-    @InjectMocks
-    private UserController userController;
-
-    @Mock
+    @MockBean
     private UserService userService;
 
     @Test
     @DisplayName("[Controller] 회원가입 테스트")
-    public void signUpTest() {
+    public void signUpTest() throws Exception {
         // given
-        UserDto userDto = UserDto.builder()
+        String request = getRequestJson(UserDto.builder()
                 .email(User_email)
                 .password(User_password)
                 .admin(User_admin)
-                .isDeleted(false)
-                .createdDate(Create_Date)
-                .creator(Creator_Member)
-                .updatedDate(Create_Date)
-                .updater(Creator_Member)
-                .build();
-        when(userService.signUp(any(UserDto.class))).thenReturn(userDto);
-
+                .build());
         // when
-        UserDto userDtoResponse = userController.signUp(userDto);
+        ResultActions actions = postRequest(baseUrl(SignUp_API), request, MediaType.APPLICATION_JSON);
 
         // then
-        assertEquals(userDto.getEmail(), userDtoResponse.getEmail());
-        verify(userService).signUp(any(UserDto.class));
+        actions.andExpect(MockMvcResultMatchers.status().isOk());
     }
 }
