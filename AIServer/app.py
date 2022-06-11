08092954dd59c13ed5b3bdb2f4dbf9745aa280f6
Oh-JunYoung <qawesdxc@naver.com>
2022-06-11 19:43:08 +0900

    Feat : AI_server
    ai cors 비동기 처리

--- AIServer/app.py
+++ AIServer/app.py
@@ -1,12 +1,35 @@
 ## uvicorn app:app --host 0.0.0.0 --port 8080
 
 from fastapi import FastAPI, WebSocket
+from fastapi.encoders import jsonable_encoder
 from CategorizationController import CategorizationController
+from fastapi.middleware.cors import CORSMiddleware
+from secrets import *
+from fastapi.concurrency import run_in_threadpool
+from celery import Celery
+
+
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=corsURL,
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+celery = Celery("CowAPI", broker=f'redis://{ip}:{port}')
+celery.conf.update()
+
+@celery.task()
+def task():
+    controller = CategorizationController()
+    return controller.returnToSpring()
+
+
 @app.get("/")
 async def root():
-    controller = CategorizationController()
-    print(controller.getResult())
-    return {"message": "Hello World"}
+    result = task()
+    return jsonable_encoder(result)
