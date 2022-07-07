225f82406483ddcf623c8598b71dd93482b8f821
oh-junyoung <qawesdxc@naver.com>
2022-07-07 10:18:43 +0900

    Feat : QnA
    Slack 알림 기능 추가

---
+++ server/src/main/java/server/server/Service/SlackService.java
@@ -0,0 +1,36 @@
+package server.server.Service;
+
+import com.slack.api.Slack;
+import com.slack.api.methods.MethodsClient;
+import com.slack.api.methods.SlackApiException;
+import com.slack.api.methods.request.chat.ChatPostMessageRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
+
+@Service
+public class SlackService {
+    @Value(value = "${slack.token}")
+    String token;
+
+    @Value(value = "${slack.channel}")
+    String channel;
+
+    public void postSlackMessage(String message) {
+        try {
+            MethodsClient methods = Slack.getInstance().methods(token);
+            ChatPostMessageRequest request = ChatPostMessageRequest.builder()
+                    .channel(channel)
+                    .text(message)
+                    .build();
+
+            methods.chatPostMessage(request);
+
+        } catch (SlackApiException | IOException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
+}
