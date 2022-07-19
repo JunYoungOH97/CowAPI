1416ca3ed66b0e06e164bf281ff4bce4c81a39aa
oh-junyoung <qawesdxc@naver.com>
2022-07-19 15:02:16 +0900

    Feat : jenkins
    젠킨스 코드 정리

---
+++ jenkins/Deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DEPLOY_PATH=/home/ec2-user/jenkins/CowAPI/server/build/libs
+
+echo ">>> Project deploy"
+cd $DEPLOY_PATH
+
+sudo chmod +x server-0.0.1-SNAPSHOT.jar
+nohup java -jar server-0.0.1-SNAPSHOT.jar &> /dev/null &
---
+++ jenkins/MovePath.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+PROJECT_PATH=/home/ec2-user/jenkins/CowAPI
+
+echo ">>> mode to path"
+cd $PROJECT_PATH
